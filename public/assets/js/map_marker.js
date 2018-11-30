4f3543f56df49f3f70b5d717852d6024951cf763
Burhan Arif M <burhan.arifm@hotmail.com>
2018-11-30 11:29:37 +0700

    added images and map marker

--- public/assets/js/map_marker.js
+++ public/assets/js/map_marker.js
@@ -1,23 +1,44 @@
-let places = [
-    {"lokasi": [-6.8874454,107.5962215], "sponsor": "WSE Paris Van Java SOHO Center"},
-    {"lokasi": [-6.225074, 106.843316], "sponsor": "WSE Kota Kasablanka Center"},
-    {"lokasi": [-6.224128, 106.809427], "sponsor": "WSE Pacific Place Center"},
-    {"lokasi": [-6.243979, 106.653879], "sponsor": "WSE Living World Center"},
-    {"lokasi": [-6.265633, 106.782871], "sponsor": "WSE Pondok Indah Center"},
-    {"lokasi": [-6.156338, 106.909266], "sponsor": "WSE Mall Kelapa Gading Center"},
-    {"lokasi": [-6.176792, 106.791271], "sponsor": "WSE Central Park Center"}
-];
+function findLocation(x,y) {
+    for (var i in places) {
+        if (places[i].lokasi[0] == x && places[i].lokasi[1] == y ) {
+            return i;
+        }
+    }
+    return -1;
+}
 
-var mymap = L.map('mapid').setView(places[0].lokasi, 13);
-//show map
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution:'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMaps</a> contributors, <ahref="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 20,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1IjoiYnVyaGFuLWFyaWZtIiwiYSI6ImNqb2JlbjJzajF5d3AzcHJzZzR6ZXdjcWYifQ.Hbh1Hm6POq6yGTHYyPa0FQ'
-}).addTo(mymap);
+function showLocation(e) {
+    let ix = findLocation(e.latlng.lat, e.latlng.lng);
+    if (ix >= 0) {
+        img.src = places[ix].gambar;
+        par.textContent = places[ix].review;
+    }
+}
+
+let gmb = document.getElementById("gmb");
+let rev = document.getElementById("review");
+let img = document.createElement('img');
+let par = document.createElement('p')
+
+gmb.appendChild(img);
+rev.appendChild(par);
+
+async function f(url) {
+    try {
+        const resp = await(fetch(url));
+        const resp2 = await resp.json();
+        localStorage.setItem('places', JSON.stringify(resp.places));
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const URL="./assets/data/map.json";
+f(URL);
+
 //add marker
+let places = JSON.parse(localStorage.getItem('places'));
 for (var center of places) {
     var marker = L.marker(center.lokasi).addTo(mymap).bindPopup(center.sponsor);
+    marker.on('click', showLocation);
 }
-var popup = L.popup();
